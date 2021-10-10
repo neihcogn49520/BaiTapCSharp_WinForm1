2d7c7ca76b1a5428d854ca5369038bbcab997965
Nguyễn Thị Ngọc Hiền <66470309+neihcogn49520@users.noreply.github.com>
2021-10-10 17:48:02 +0700

    label

---
+++ Label/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Label
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.first_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // first_label
+            // 
+            this.first_label.AutoSize = true;
+            this.first_label.Location = new System.Drawing.Point(115, 82);
+            this.first_label.Name = "first_label";
+            this.first_label.Size = new System.Drawing.Size(0, 17);
+            this.first_label.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 319);
+            this.Controls.Add(this.first_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label first_label;
+    }
+}
+
---
+++ Label/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Label
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            first_label.Text = "This is my first label";
+            first_label.BorderStyle = BorderStyle.FixedSingle;
+            first_label.TextAlign = ContentAlignment.MiddleCenter;
+        }
+    }
+}
