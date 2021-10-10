3041e1531b2ec6f2871496d4e95c1e73a7c0d085
Nguyễn Thị Ngọc Hiền <66470309+neihcogn49520@users.noreply.github.com>
2021-10-10 17:47:30 +0700

    label

---
+++ Hello World/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Hello_World
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click this";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbHelloWorld
+            // 
+            this.lbHelloWorld.AutoSize = true;
+            this.lbHelloWorld.Location = new System.Drawing.Point(278, 102);
+            this.lbHelloWorld.Name = "lbHelloWorld";
+            this.lbHelloWorld.Size = new System.Drawing.Size(0, 17);
+            this.lbHelloWorld.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 314);
+            this.Controls.Add(this.lbHelloWorld);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbHelloWorld;
+    }
+}
+
