d58950df4a9df00bb0faa764cd38488cfa0661ea
Nguyễn Thị Ngọc Hiền <66470309+neihcogn49520@users.noreply.github.com>
2021-10-10 17:46:25 +0700

    combobox control

---
+++ ComboBox/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbox1 = new System.Windows.Forms.ComboBox();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbox1
+            // 
+            this.cbox1.FormattingEnabled = true;
+            this.cbox1.Items.AddRange(new object[] {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thusday",
+            "Friday",
+            "Saturday",
+            "Sunday"});
+            this.cbox1.Location = new System.Drawing.Point(192, 139);
+            this.cbox1.Name = "cbox1";
+            this.cbox1.Size = new System.Drawing.Size(169, 24);
+            this.cbox1.TabIndex = 0;
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(243, 228);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(77, 33);
+            this.bt1.TabIndex = 1;
+            this.bt1.Text = "Select";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 446);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.cbox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbox1;
+        private System.Windows.Forms.Button bt1;
+    }
+}
+
