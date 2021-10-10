ee1558cc8e9d34c1ff8cc5c96487e817a51f9718
Nguyễn Thị Ngọc Hiền <66470309+neihcogn49520@users.noreply.github.com>
2021-10-10 17:48:31 +0700

    textbox control

---
+++ TextBox/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace TextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(198, 131);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(290, 22);
+            this.tb1.TabIndex = 0;
+            this.tb1.Text = "Nguyễn Thị Ngọc Hiền";
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(198, 227);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(90, 38);
+            this.bt1.TabIndex = 1;
+            this.bt1.Text = "Submit";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 432);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.tb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Button bt1;
+    }
+}
+
---
+++ TextBox/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tb1.Width = 250;
+            tb1.Height = 50;
+            tb1.Multiline = true;
+            tb1.BackColor = Color.LightBlue;
+            tb1.ForeColor = Color.White;
+            tb1.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            string var;
+            var = tb1.Text;
+            MessageBox.Show(var);
+        }
+    }
+}
